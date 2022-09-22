2a9be2a64b25fee85cbf305544b99ecb26072047
Jakub Soboczyński <teziovsky@gmail.com>
2022-09-22 11:20:49 +0200

    feat(light-bulb): extract class, add types and methods

---
+++ week-1-ts-ecosystem/w01e04-light-me-up/src/lightBulb.ts
@@ -0,0 +1,18 @@
+ import { PowerSource } from "./powerSource";
+
+export class LightBulb {
+  protected readonly powerConsumption = 20;
+
+  constructor(private powerSource: PowerSource) {}
+
+  turnOn() {
+    this.powerSource.consume(this.powerConsumption);
+    console.log("🔦");
+  }
+
+  turnOnDuring(seconds: number) {
+    for (let second = 0; second < seconds; second++) {
+      this.turnOn();
+    }
+  }
+}
