c52e9ae8c04d51b8196973f433e4dd65c38e00dc
Jakub Soboczyński <teziovsky@gmail.com>
2022-09-23 15:04:21 +0200

    refactor(types): extract User to models

---
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/models/user.d.ts
@@ -0,0 +1,8 @@
+import { Address } from "../types";
+
+export interface User {
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+}
--- week-1-ts-ecosystem/w01e01-typescript-basics/src/types/index.d.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/types/index.d.ts
@@ -1,11 +1,4 @@
-interface Address {
+export interface Address {
   street: string;
   no: number;
-}
-
-export interface User {
-  name: string;
-  age: number;
-  email: string;
-  address: Address;
 }
