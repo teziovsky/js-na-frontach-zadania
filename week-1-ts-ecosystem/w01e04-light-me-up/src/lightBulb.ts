ad54c2e75c567c6cc0d1e8bbe6960a41c7c00597
Jakub Soboczyński <teziovsky@gmail.com>
2022-10-03 17:26:32 +0200

    refactor: update ex2 with Promise

--- week-1-ts-ecosystem/w01e04-light-me-up/src/lightBulb.ts
+++ week-1-ts-ecosystem/w01e04-light-me-up/src/lightBulb.ts
@@ -1,20 +1,36 @@
- import { PowerSource } from "./powerSource";
+import { PowerSource } from "./powerSource";
 
 export class LightBulb {
   protected readonly powerConsumption = 20;
 
-  constructor(private powerSource: PowerSource) {}
-
-  turnOn() {
-    this.powerSource.consume(this.powerConsumption);
-    console.log("🔦");
+  constructor(private powerSource: PowerSource) {
   }
 
-  turnOnDuring(seconds: number) {
+  turnOn() {
+    try {
+      this.powerSource.consume(this.powerConsumption);
+      console.log("🔦");
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      } else {
+        console.error("Unknown error", error);
+      }
+    }
+  }
+
+  async turnOnDuring(seconds: number) {
     for (let second = 0; second < seconds; second++) {
-      setTimeout(() => {
-        this.turnOn();
-      }, 1000 * second)
+      await new Promise<void>(async (resolve, reject) => {
+        await setTimeout(() => {
+          try {
+            this.turnOn();
+            resolve();
+          } catch (error) {
+            reject(error);
+          }
+        }, 1000);
+      });
     }
   }
 }
--- week-1-ts-ecosystem/w01e04-light-me-up/src/main.ts
+++ week-1-ts-ecosystem/w01e04-light-me-up/src/main.ts
@@ -8,14 +8,55 @@
  * Dodatkowo — rzucać błąd, jeśli zapas mocy się wyczerpie.
  * */
 
-class PowerSource {
-  private energySupply = 100
+import { LightBulb } from "./lightBulb";
+import { PowerSource } from "./powerSource";
 
-  consume(energy) {
-    this.energySupply -= energy
+(() => {
+  try {
+    console.log("Excercise 1");
+
+    const powerSource = new PowerSource();
+
+    const lightBulb1 = new LightBulb(powerSource);
+    lightBulb1.turnOn();
+
+    const lightBulb2 = new LightBulb(powerSource);
+    lightBulb2.turnOn();
+
+    const lightBulb3 = new LightBulb(powerSource);
+    lightBulb3.turnOn();
+
+    const lightBulb4 = new LightBulb(powerSource);
+    lightBulb4.turnOn();
+
+    const lightBulb5 = new LightBulb(powerSource);
+    lightBulb5.turnOn();
+
+    const lightBulb6 = new LightBulb(powerSource);
+    lightBulb6.turnOn();
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("Unknown error", error);
+    }
   }
-}
+})();
 
-class LightBulb {
-  protected readonly powerConsumption = 20
-}
+(async () => {
+  try {
+    console.log();
+    console.log("Excercise 2");
+
+    const powerSource = new PowerSource();
+
+    const lightBulb1 = new LightBulb(powerSource);
+    await lightBulb1.turnOnDuring(6);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error("Unknown error", error);
+    }
+  }
+})();
