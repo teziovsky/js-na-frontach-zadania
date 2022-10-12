b288ffaa6037e478a78e3a6a090f56bfc87f7bf3
Jakub Soboczyński <teziovsky@gmail.com>
2022-10-12 19:11:26 +0200

    feat(pagination): add fromObject parameter

--- week-2-wide-perspective/w02e03-pagination-problem/src/middlewares/pagination.middleware.js
+++ week-2-wide-perspective/w02e03-pagination-problem/src/middlewares/pagination.middleware.js
@@ -1,6 +1,6 @@
-export function paginationMiddleware(skipName = 'skip', limitName = 'limit') {
+export function paginationMiddleware(skipName = 'skip', limitName = 'limit', fromObject = "query") {
   return (req, res, next) => {
-    const { [skipName]: skip, [limitName]: limit } = req.query
+    const { [skipName]: skip, [limitName]: limit } = req[fromObject]
     let computedSkip = 0
     if (skip) {
       computedSkip = Number(skip)
