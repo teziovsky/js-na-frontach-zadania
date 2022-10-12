2782d571a500f4be1b64df9ad3b644c677da9eb6
Jakub Soboczyński <teziovsky@gmail.com>
2022-10-12 19:06:05 +0200

    refactor(main): update main script

--- week-2-wide-perspective/w02e02-data-manipulation/src/main.js
+++ week-2-wide-perspective/w02e02-data-manipulation/src/main.js
@@ -1,8 +1,26 @@
-import { ordersFakeData } from './orders/orders.fake-data.js'
+import { annualOrderTile } from "./orders/components/AnnualOrderTile.js";
+import { orderTile } from "./orders/components/OrderTile.js";
 
-import './orders/tiles/BestSaleTile.js'
-import './orders/tiles/TotalOrdersTile.js'
-import './orders/tiles/TotalIncomeTile.js'
-import './orders/tiles/AnnualOrdersTile.js'
+import OrdersService from "./orders/orders.service.js";
 
-console.log(ordersFakeData)
+async function main() {
+  const year = 2022;
+  const orders = await OrdersService.getOrders();
+  const totalOrders = OrdersService.getTotalOrders(orders);
+  const totalIncomeOrders = OrdersService.getTotalIncome(orders);
+  const bestSale = OrdersService.getBestSale(orders);
+  const orders2022 = OrdersService.getOrdersByYear(orders, year);
+
+  await orderTile("best-sale", bestSale);
+  await orderTile("total-orders", totalOrders);
+  await orderTile("total-income", totalIncomeOrders);
+  await annualOrderTile("annual-orders", year, orders2022);
+}
+
+const button = document.querySelector("[data-button]");
+button.addEventListener("click", async () => {
+  await OrdersService.addOrder();
+  await main();
+});
+
+await main();
