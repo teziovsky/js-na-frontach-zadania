79eabd4d2fe9d6b11b63e4bffbef69b5cf9b4f19
Jakub Soboczyński <teziovsky@gmail.com>
2022-10-18 21:04:19 +0200

    feat(divider): add tests

--- week-4-tests-and-persistence/w04e01-simple-testing/src/app/divider.js
+++ week-4-tests-and-persistence/w04e01-simple-testing/src/app/divider.js
@@ -2,6 +2,8 @@
  * Tę funkcjonalność chcemy przetestować:
  * */
 export function divider(dividend, divisor) {
+  if (typeof divisor !== "number") throw new Error("Divisor must be a number");
+  if (typeof dividend !== "number") throw new Error("Dividend must be a number");
   if (divisor === 0) throw new Error("Cannot divide by 0");
   return dividend / divisor;
 }
--- week-4-tests-and-persistence/w04e01-simple-testing/src/app/divider.spec.js
+++ week-4-tests-and-persistence/w04e01-simple-testing/src/app/divider.spec.js
@@ -6,6 +6,19 @@ describe("divider", () => {
     expect(result).toBe(5);
   });
 
+  it("should return 0 if dividend is 0", () => {
+    const result = divider(0, 2);
+    expect(result).toBe(0);
+  });
+
+  it("should throw an error when divisor is not number", () => {
+    expect(() => divider(10, "a")).toThrowError();
+  });
+
+  it("should throw an error when dividend is not number", () => {
+    expect(() => divider("a", 10)).toThrowError();
+  });
+
   it("should throw an error when divisor is 0", () => {
     expect(() => divider(10, 0)).toThrowError();
   });
