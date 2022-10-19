b43e9b1ff3593702be297b297bf646ff59da99bf
Jakub Soboczyński <teziovsky@gmail.com>
2022-10-19 19:05:53 +0200

    feat: add dist folder

---
+++ week-5-ci-and-production/w05e01-npm-pipeline/dist/app/shopping-lists/ProductItem.js
@@ -0,0 +1,20 @@
+import { defineComponent } from "vue";
+export default defineComponent({
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        },
+    },
+    template: `
+    <div class="panel-block" 
+         v-if="product" 
+         :style="{ textDecoration: product.status === 'BOUGHT' ? 'line-through' : '' }"
+    >
+      <div >{{product.name}}</div>
+      <div class="ml-auto">{{product.quantity}}</div>
+      <div class="tag">{{product.unit}}</div>
+      <div class="ml-4">{{product.price}} PLN</div>
+    </div>
+  `,
+});
