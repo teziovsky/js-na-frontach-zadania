6c5e387adbdec7b2ce375fb29231dfb06c482427
Jakub Soboczyński <teziovsky@gmail.com>
2022-11-06 18:37:06 +0100

    fix: scripts

--- week-5-ci-and-production/w05e01-npm-pipeline/dist/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import AppLayout from "./app/AppLayout";
-import { router } from "./routes";
-const app = createApp(AppLayout);
-app.use(router);
-app.mount("#app");
