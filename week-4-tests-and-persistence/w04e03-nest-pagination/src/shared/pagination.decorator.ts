c3aebb63ef4a54ca55e87d4470d82165975c4a72
Jakub Soboczyński <teziovsky@gmail.com>
2022-11-06 18:26:58 +0100

    refactor(pagination): extract magic number to const

--- week-4-tests-and-persistence/w04e03-nest-pagination/src/shared/pagination.decorator.ts
+++ week-4-tests-and-persistence/w04e03-nest-pagination/src/shared/pagination.decorator.ts
@@ -2,17 +2,20 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 import { Pagination } from './Pagination';
 
+const DEFAULT_LIMIT = 10;
+
 export const GetPagination = createParamDecorator(
   (data, ctx: ExecutionContext): Pagination => {
     const req: Request = ctx.switchToHttp().getRequest();
 
     const paginationParams: Pagination = {
       skip: 0,
-      limit: 10,
+      limit: DEFAULT_LIMIT,
     };
 
     paginationParams.skip = parseInt(req?.query?.skip?.toString()) || 0;
-    paginationParams.limit = parseInt(req?.query?.limit?.toString()) || 10;
+    paginationParams.limit =
+      parseInt(req?.query?.limit?.toString()) || DEFAULT_LIMIT;
 
     return paginationParams;
   },
