29073c37ed71bebfc8c3d9ba856560dbf0318656
Jakub Soboczyński <teziovsky@gmail.com>
2022-11-06 18:23:35 +0100

    refactor(tests): add tests with float values and update throw messages

--- week-4-tests-and-persistence/w04e01-simple-testing/src/app/divider.spec.js
+++ week-4-tests-and-persistence/w04e01-simple-testing/src/app/divider.spec.js
@@ -3,23 +3,33 @@ import { divider } from "./divider";
 describe("divider", () => {
   it("should divide two numbers", () => {
     const result = divider(10, 2);
-    expect(result).toBe(5);
+    expect(result).toBe("5");
+  });
+
+  it("should divide numbers with float value", () => {
+    const result = divider(10, 2.5);
+    expect(result).toBe("4");
+  });
+
+  it("should divide two numbers with float value", () => {
+    const result = divider(5.2,  0.81, 5);
+    expect(result).toBe("6.41975");
   });
 
   it("should return 0 if dividend is 0", () => {
     const result = divider(0, 2);
-    expect(result).toBe(0);
+    expect(result).toBe("0");
   });
 
   it("should throw an error when divisor is not number", () => {
-    expect(() => divider(10, "a")).toThrowError();
+    expect(() => divider(10, "a")).toThrowError("Divisor must be a number");
   });
 
   it("should throw an error when dividend is not number", () => {
-    expect(() => divider("a", 10)).toThrowError();
+    expect(() => divider("a", 10)).toThrowError("Dividend must be a number");
   });
 
   it("should throw an error when divisor is 0", () => {
-    expect(() => divider(10, 0)).toThrowError();
+    expect(() => divider(10, 0)).toThrowError("Cannot divide by 0");
   });
 });
