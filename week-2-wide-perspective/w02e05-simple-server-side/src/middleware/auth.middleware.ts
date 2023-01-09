6d74d7b57d99df1ea72c3ae6a0d62128e7cf3582
Jakub Soboczyński <teziovsky@gmail.com>
2023-01-09 19:27:38 +0100

    refactor(auth): replace plain text to json response

--- week-2-wide-perspective/w02e05-simple-server-side/src/middleware/auth.middleware.ts
+++ week-2-wide-perspective/w02e05-simple-server-side/src/middleware/auth.middleware.ts
@@ -5,7 +5,7 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    res.status(401).send("Unauthorized");
+    res.status(401).json({ message: "Unauthorized" });
   } else {
     const [_, token] = authHeader.split(" ");
     const { JWT_SECRET_TOKEN = "supersecret" } = process.env;
@@ -14,7 +14,7 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
     const { id, email } = decodedToken as { id: number; email: string };
 
     if (!decodedToken) {
-      res.status(401).send("Unauthorized");
+      res.status(401).json({ message: "Unauthorized" });
     } else {
       res.locals.user = {
         id,
